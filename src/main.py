32ae8e53abf6b8d570390d1be117ca07481d84da
paco.mendes <paco.mendes@82bd030b-c855-0410-b6cd-c3387e35c2a2>
2008-09-01 06:52:17 +0000

    renamed src

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db 
+from django.utils import simplejson
 
 def toNum(s):
     """Convert string to either int or float."""
@@ -24,14 +25,7 @@ class journeys(db.Model):
     end = db.GeoPtProperty()
     user = db.ReferenceProperty(users, collection_name='user_journeys')
     
-class AddUser(webapp.RequestHandler):
-    def post(self):
-        user = users()
-        user.email = db.Email(self.request.get('email'))
-        user.put()
-        user.ukey = str(user.key())
-        user.put()
-        self.redirect('/display')
+
 
 class AddJourney(webapp.RequestHandler):
     def post(self):
@@ -119,8 +113,51 @@ class JSON(webapp.RequestHandler):
     self.response.out.write('{"validated":"This request was spoofed","query":[],"url":"http:\/\/graargh.returnstrue.com\/buh\/fetchme.php","signature":"","signature_len":1}')
     self.response.headers.add_header("Content-Type", "text/javascript")
 
+class RPCHandler(webapp.RequestHandler):
+  """ Allows the functions defined in the RPCMethods class to be RPCed."""
+
+  def __init__(self):
+    webapp.RequestHandler.__init__(self)
+    self.methods = RPCMethods()
+    
+  def get(self):
+    func = None
+    action = self.request.get('action')
+    if action:
+      if action[0] == '_':
+        self.error(403) # access denied
+        return
+      else:
+        func = getattr(self.methods, action, None)
+   
+    if not func:
+      self.error(404) # file not found
+      return
+     
+    args = ()
+    while True:
+      key = 'arg%d' % len(args)
+      val = self.request.get(key)
+      if val:
+        args += (simplejson.loads(val),)
+      else:
+        break
+    result = func(*args)
+    self.response.out.write(simplejson.dumps(result))
+
+class RPCMethods:
+  """ Defines the methods that can be RPCed.
+  NOTE: Do not allow remote callers access to private/protected "_*" methods.
+  """
+  def Add(self, *args):
+    # The JSON encoding may have encoded integers as strings.
+    # Be sure to convert args to any mandatory type(s).
+    ints = [int(arg) for arg in args]
+    return sum(ints)
+
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
+                                      ('/rpc', RPCHandler),
                                       ('/addd',AddData),
                                       ('/addu',AddUser),
                                       ('/addj',AddJourney),
