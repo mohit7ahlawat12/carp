340dfad2ce63d2ecc48f4fc8bfbbe772c23b38d5
paco.mendes <paco.mendes@82bd030b-c855-0410-b6cd-c3387e35c2a2>
2008-09-15 20:26:48 +0000

    Updates to journey matching algorithm.

--- trunk/src/journeys.py
+++ trunk/src/journeys.py
@@ -1,7 +1,8 @@
 from google.appengine.ext import db
 from math import acos,sin,cos,radians
-import users
+from decimal import * 
 
+import users
 
 class journeys(db.Model):
     jkey = db.StringProperty()
@@ -23,62 +24,103 @@ class JourneyManager():
         journey.put()
         return str(journey.key())
     
-    def GetSimilarJourneys(self,jkey):
-        refj = journeys.get(db.Key(jkey))
-        q = db.Query()
+    #@param jkey: string key of reference journey to find matches
+    #@param num: number of journeys to return
+    #@param units: units "km" or "miles"
+    #@param threshold: maximum distance between either origin or destination
+    def GetSimilarJourneys(self,jkey,num,units,threshold):
         
+        #list of resulting journeys
+        results = JourneyList(num)
+        
+        refj = journeys.get(db.Key(jkey))
+        #q = db.Query()
+        
+        #get co-ordinates of reference journey
         orig_lat = refj.start.lat
         orig_lon = refj.start.lon
-        
         dest_lat = refj.end.lat
         dest_lon = refj.end.lon
         
         q = journeys.all()
         for j in q:
-            distance = distancePts(orig_lat,orig_lon,j.start.lat,j.start.lon,'km')
+            #TODO: perform rough estimate first (improve performance)
+            #perform more accurate calculation
+            start_disp = distancePts(orig_lat,orig_lon,j.start.lat,j.start.lon,units)
+            end_disp = distancePts(dest_lat,dest_lon,j.end.lat,j.end.lon,units)
+            total_distance = start_distance + end_distance
+            
+            jour_disp = JourneyDisp(j,start_disp,end_disp)
+            
+            if(jour_disp.total_disp < threshold):
+                #Update reference list
+                results.insert(jour_disp)
         
+        return results
+    
     def distancePts(self,lat1,lon1,lat2,lon2,units):
         if units == 'km':
             factor = 6371.0
         elif units == 'miles':
             factor = 3959.0
-        return (factor*acos(cos(radians(lat1))*cos(radians(lat2))*cos(radians(lon2) - radians(lon1)) + sin(radians(lat1)) * sin(radians( lat2))))
+            #The Haversine formula
+            #http://www.movable-type.co.uk/scripts/latlong.html
+            #http://code.google.com/apis/maps/articles/phpsqlsearch.html#findnearsql
+        
+        distance = (factor*acos(cos(radians(lat1))*cos(radians(lat2))*cos(radians(lon2) - radians(lon1)) + sin(radians(lat1)) * sin(radians( lat2))))
 
-class RefMarker:
-    def __init__(self,jour,disp, start):
-        self.jour = jour
-        self.disp = disp
-        self.start = start
-
+        #set precision
+        getcontext().prec = 4
+        return Decimal(str(distance))
+     
+#Simple container object to store a journey reference and the total displacement for distance comparison
+class JourneyDisp:
+    #@param journey: a journey being compared
+    #@param total_disp: Total displacement of this journey from the reference journey 
+    def __init__(self,journey,start_disp,end_disp):
+        self.journey = journey
+        self.start_disp = start_disp
+        self.end_disp = end_disp
+        self.total_disp = start_disp + end_disp
+        
 #Container of journeys, limited in size. Where limit is reached, largest item is removed. Inserts in order or increasing distance 
 #if disp <= range: jl.insert(JourDisp(j,disp))
+#Class ensures that the top journeys are returned in order
 class JourneyList:
     def __init__(self,capacity):
+        #maximum number of journeys
         self.capacity = capacity
-        self.journeys = []
-        self.max_disp = None
+        #container of resulting journeys
+        self.journey_list = []
+        #maximum acceptable displacement between points
+        self._max_disp = None
         
-    def insert(self,ref_marker):
-        if self.max_disp == None:
+    def insert(self,jour_disp):
+        if self._max_disp == None:
             #First item in list
-            journeys.append(ref_marker)
-            max_disp = ref_marker.disp
+            self.journeys.append(jour_disp)
+            #update the maximum displacement
+            self.max_disp = jour_disp.total_disp
             return True
         else:
-            if (ref_marker.disp > max_disp) and (self.journeys.len() < self.limit):
+            if (self.journeys.len() < self.capacity):
                 #new max distance item, but no need to pop largest from list
-               self.__orderedInsert(ref_marker)
-               
-            elif (distance < max_dist) and (self.journeys.len() >= self.limit):
-                #capacity reached, pop largest item off
+               self.__orderedInsert(jour_disp)
+            elif (jour_disp.total_disp < max_dist) and (self.journeys.len() >= self.capacity):
+                #shorter journey, capacity reached, pop largest item off
                 self.__orderedInsert(jour_disp)
                 journeys.pop()
 
     #max: append, otherwise insert
     def __orderedInsert(self,jour_disp):
         pos = 0
-        for jd in journeys:
-            if jour_disp.disp > jd.disp:
-                journeys.insert(x,jour_disp)
+        for jd in self.journey_list:
+            if jour_disp.total_disp > jd.total_disp:
+                journeys.insert(pos,jour_disp)
                 break
             pos += 1
+            
+        if(self.max_disp < jour_disp.total_disp):
+            #update the maximum displacement
+            self.max_disp = jour_disp.total_disp
+            
